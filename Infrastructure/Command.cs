7e5cd981b4597b2157c32dc90a0bcad397794db3
Anton Lugom <mogul086@mail.ru>
2023-03-26 12:01:29 +0300

    Настрофка каталогов и базовой инфраструктуры

---
+++ Infrastructure/Command.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Infrastructure
+{
+    public class Command : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?> _canExecute;
+
+        public Command(Action<object?> execute, Predicate<object?> canExecute = null!)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+            => _canExecute?.Invoke(parameter!) ?? true;
+
+        public void Execute(object? parameter)
+            => _execute?.Invoke(parameter!);
+    }
+}
